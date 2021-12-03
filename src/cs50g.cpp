de0a0edbcc13f80b8e8f73da0d316c19b9dbe517
Kyle Coffey <mariorules24@gmail.com>
2021-12-03 04:58:43 -0600

    formatting, font

--- src/cs50g.cpp
+++ src/cs50g.cpp
@@ -1,12 +1,46 @@
 ﻿// cs50g.cpp : Defines the entry point for the application.
 //
 
-#include <iostream>
-#include <SDL.h>
 #include "GraphicsContext.hpp"
+#include "MySDL.hpp"
+#include "SmartRenderer.hpp"
+#include "SmartWindow.hpp"
+#include <SDL.h>
+#include <SDL_ttf.h>
+#include <iostream>
+
+constexpr int WINDOW_WIDTH = 1280;
+constexpr int WINDOW_HEIGHT = 720;
 
 int main(int /*argc*/, char** /*argv*/)
 {
-	GraphicsContext gc;
-	return 0;
+    const GraphicsContext gc;
+    TTF_Font* font = mysdl::CheckPointer(TTF_OpenFont("res/font.ttf", 12));
+    const SmartWindow window{"Pong",       SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                             WINDOW_WIDTH, WINDOW_HEIGHT,           SDL_WINDOW_ALLOW_HIGHDPI};
+    const SmartRenderer renderer{window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC};
+
+    bool run = true;
+    while (run)
+    {
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+            case SDL_QUIT:
+                run = false;
+                break;
+            default:
+                break;
+            }
+        }
+
+        renderer.setDrawColor(SDL_Color{0, 0, 0, 255});
+        renderer.clear();
+        renderer.present();
+    }
+
+    TTF_CloseFont(font);
+    return 0;
 }
